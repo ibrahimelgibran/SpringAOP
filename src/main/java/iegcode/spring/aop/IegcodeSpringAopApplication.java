20115b79b5a65fd061652c7fedc35a230bf7dcfd
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-10 16:08:56 +0700

    first commit

---
+++ src/main/java/iegcode/spring/aop/IegcodeSpringAopApplication.java
@@ -0,0 +1,13 @@
+package iegcode.spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IegcodeSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IegcodeSpringAopApplication.class, args);
+	}
+
+}
