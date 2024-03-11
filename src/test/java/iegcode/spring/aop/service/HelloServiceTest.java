ea926dd718df26f96648597d7e96f491ee7a6de3
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-11 16:39:58 +0700

    update

---
+++ src/test/java/iegcode/spring/aop/service/HelloServiceTest.java
@@ -0,0 +1,19 @@
+package iegcode.spring.aop.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloServiceTest {
+
+    @Autowired
+    private HelloService helloService;
+
+    @Test
+    void helloService() {
+        Assertions.assertEquals("Hello Gibran", helloService.hello("Gibran"));
+        Assertions.assertEquals("bye Gibran", helloService.bye("Gibran"));
+    }
+}
