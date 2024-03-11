e7082b1954994169e24d10d562d393c1a9f7d12c
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-11 17:29:19 +0700

    update

--- src/main/java/iegcode/spring/aop/aspect/LogAspect.java
+++ src/main/java/iegcode/spring/aop/aspect/LogAspect.java
@@ -1,6 +1,9 @@
 package iegcode.spring.aop.aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -17,8 +20,25 @@ public class LogAspect {
     }
 
     @Before("helloServiceMethod()")
-    public void beforeHelloServiceMethod(){
-      log.info("Before HelloService Method");
+    public void beforeHelloServiceMethod(JoinPoint joinPoint){
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("Before " + className + "." + methodName + "()");
+    }
+
+    @Around("helloServiceMethod()")
+    public Object aroundHelloServiceMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        try {
+            log.info("Around Before " + className + "." + methodName + "()");
+            return joinPoint.proceed(joinPoint.getArgs());
+        }catch (Throwable throwable){
+            log.info("Around Error " + className + "." + methodName + "()");
+            throw throwable;
+        } finally {
+            log.info("Around Finally " + className + "." + methodName + "()");
+        }
     }
 
 }
